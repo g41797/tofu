d6295e5f1d9bef98a821fa98f0a2ce8c8fe53769
g41797 <g41797@gmail.com>
2025-09-01 11:47:49 +0300

    Add kqueue for macOS Notifier - Chat gpt porting of former code

--- src/all_tests.zig
+++ src/all_tests.zig
@@ -4,13 +4,13 @@
 // pub const std_options: @import("std").Options = .{ .log_level = .debug };
 
 test {
-    _ = @import("message_tests.zig");
-    _ = @import("configurator_tests.zig");
-    _ = @import("engine_test.zig");
-    _ = @import("engine/Pool_tests.zig");
     _ = @import("engine/Notifier_tests.zig");
-    _ = @import("engine/channels_tests.zig");
-    _ = @import("engine/sockets_tests.zig");
+    // _ = @import("message_tests.zig");
+    // _ = @import("configurator_tests.zig");
+    // _ = @import("engine_test.zig");
+    // _ = @import("engine/Pool_tests.zig");
+    // _ = @import("engine/channels_tests.zig");
+    // _ = @import("engine/sockets_tests.zig");
     @import("std").testing.refAllDecls(@This());
 }
 
