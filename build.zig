65008b657e816a67556cdc4e6e63811191fb0392
g41797 <g41797@gmail.com>
2025-09-09 15:51:16 +0300

    Move tests to separate folder

--- build.zig
+++ build.zig
@@ -76,7 +76,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/tofu_tests.zig"),
+        .root_source_file = b.path("tests/tofu_tests.zig"),
         .target = target,
         .optimize = optimize,
         .single_threaded = false,
