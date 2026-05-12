1dd5b1e94da8e826d9fccd62e9550da4838b0716
g41797 <g41797@gmail.com>
2026-05-12 21:19:54 +0300

    Mac CI investigation

--- tests/tofu_tests.zig
+++ tests/tofu_tests.zig
@@ -22,36 +22,38 @@ test {
         _ = @import("ampe/sockets_tests.zig");
     }
 
+    // posix_net module contract tests :
+    if (test_gate_options.portable) {
+        _ = @import("posix_net/posix_net_tests.zig");
+    }
+
     if (@import("builtin").os.tag != .macos) {
 
         // posix_net module contract tests :
         if (test_gate_options.portable) {
-            _ = @import("posix_net/posix_net_tests.zig");
             _ = @import("ampe/portable_poller_tests.zig");
         }
 
         // Poller backend contract tests (backend-independent, all platforms):
-    _ = @import("ampe/poller_tests.zig");
+        _ = @import("ampe/poller_tests.zig");
 
         // PollerCore integration tests (all backends, all platforms):
-    _ = @import("pollercore_tests.zig");
+        _ = @import("pollercore_tests.zig");
 
         std.log.debug("\r\n   ****  tofu TESTS Reactor ****\r\n", .{});
 
-        // Main test - for Linux
-    // if (@import("builtin").os.tag == .linux) {
-    _ = @import("reactor_tests.zig");
-        // }
-
+        _ = @import("reactor_tests.zig");
     }
 
     @import("std").testing.refAllDecls(@This());
 }
 
-const tofu = @import("tofu");
-const recipes = @import("recipes");
-const test_gate_options = @import("test_gate_options");
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const gpa = std.testing.allocator;
+
+const recipes = @import("recipes");
+const test_gate_options = @import("test_gate_options");
+const tofu = @import("tofu");
+
